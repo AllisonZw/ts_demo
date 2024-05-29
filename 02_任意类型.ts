ec68fa42df43f93c2d3464431e65beae862f9cf3
dzw <1756644571@qq.com>
2024-05-29 21:37:44 +0800

    first commit

---
+++ 02_任意类型.ts
@@ -0,0 +1,14 @@
+// any 任意类型 unknown 不知道的类型
+// 1. top type 顶级类型 any unknown
+// 2. Object
+// 3. Number String Boolean Symbol
+// 4. number string boolean symbol
+// 5. 123 "hello" true false null undefined
+// 6. never
+// *unknow 只能赋值给自身(unknow)或者是any
+// unknow 没有办法读取任何属性 方法也不可以调用
+// unknow 比 any 更加安全
+
+let xiaowen: any = { shuai: true, open: () => 123 }
+console.log(xiaowen.shuai)
+// 123
