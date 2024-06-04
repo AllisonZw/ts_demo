2802fb2020815be8a5c2c3e45abc1b3cda94b28b
dzw <1756644571@qq.com>
2024-06-04 10:28:48 +0800

    类型推论、别名

---
+++ 13_类型推论、别名.ts
@@ -0,0 +1,25 @@
+// let str = 'hello world'
+// let arr = [1, 2, 3]
+// let str1
+// str1 = 123
+// str1 = 'hello world'
+// str1 = null
+
+// type s = (number[] & B) | string
+// interface A extends B {
+// 	name: string | number
+// }
+// // interface还可以重名，等价于  补充
+// interface B {
+// 	age: number
+// }
+
+// extends 包含的意思
+// 左边的值 会作为右边类型的子类型
+// any unknow
+// Object
+// Number String Boolean
+// number string boolean
+// 1 'test' true
+// never
+// type num = 1 extends number ? 1 : 0
